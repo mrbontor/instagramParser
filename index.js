8ea85503115533f14b64ebbafcf289466ee12dae
mrbontor <mrbontor@gmail.com>
2020-07-04 00:50:19 +0700

    fixxing flow request

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const needle = require('needle');
+
+const port = process.env.PORT || 3000;
+
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/ig', function (req, res) {
-	getMedia(req.params.username)
-	.then(data => {
-		res.status(200).json(data)
-	})
-	.catch(e => {
-		res.status(200).json(e)
-	})
-})
 
 app.get('/', function (req, res) {
 	res.json("Surya Pro")
 })
 
-app.listen(8000)
+app.get('/ig/:username', function (req, res) {
+    let user = req.params.username;
+
+    getMedia(user)
+    .then(data => {
+        res.status(200).json(data)
+    })
+    .catch(e => {
+        res.status(200).json(e)
+    })
+})
 
 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 function getMedia(username) {
     return new Promise(function(resolve, reject) {
@@ -27,7 +37,7 @@ function getMedia(username) {
             follow_max: 5
         }
         needle.get(`https://www.instagram.com/${username}/?__a=1`, options, function(error, response, body) {
-            console.log(`[getMediaResponse] >>>>  ${JSON.stringify(body)}`)
+            // console.log(`[getMediaResponse] >>>>  ${JSON.stringify(body)}`)
             if (error) reject({
                 msg: 'something wrong'
             })
